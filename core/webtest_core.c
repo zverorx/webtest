7b45ef5c9401f81bf5a00d4f0f2f1982cb17c6af
zverorx <zveror1806@gmail.com>
2026-03-12 18:39:58 +0300

    docs: update copyright year from 2025 to 2026

--- core/webtest_core.c
+++ core/webtest_core.c
@@ -2,7 +2,7 @@
  * This file is part of webtest.
  * webtest - web server for speed testing.
  *
- * Copyright (C) 2025 Egorov Konstantin
+ * Copyright (C) 2026 Egorov Konstantin
  *
  * webtest is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
